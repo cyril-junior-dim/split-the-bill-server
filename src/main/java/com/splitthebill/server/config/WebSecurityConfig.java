3d82c4662e5a8e2671c202891715d93425684045
Kacper Wojcikowski <wojcikowskikacper5@gmail.com>
2021-01-17 20:12:19 +0100

    Implemented TLS/SSL support.

--- src/main/java/com/splitthebill/server/config/WebSecurityConfig.java
+++ src/main/java/com/splitthebill/server/config/WebSecurityConfig.java
@@ -61,6 +61,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests().antMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated();
 
+        http.requiresChannel().anyRequest().requiresSecure();
+
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
