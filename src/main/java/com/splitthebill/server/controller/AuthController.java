b4bbfb5c1cf23445f66571b5f3893ea6da93c2bc
Kacper Wojcikowski <wojcikowskikacper5@gmail.com>
2021-01-17 19:35:36 +0100

    Added oauth2 third party authentication endpoint

--- src/main/java/com/splitthebill/server/controller/AuthController.java
+++ src/main/java/com/splitthebill/server/controller/AuthController.java
@@ -3,23 +3,23 @@ package com.splitthebill.server.controller;
 import com.splitthebill.server.dto.JwtResponseDto;
 import com.splitthebill.server.dto.LoginRequestDto;
 import com.splitthebill.server.dto.BasicUserAccountCreateDto;
+import com.splitthebill.server.dto.ThirdPartyUserAccountCreateDto;
+import com.splitthebill.server.model.user.ThirdPartyUserAccount;
 import com.splitthebill.server.security.JwtUtils;
-import com.splitthebill.server.security.UserDetailsImpl;
 import com.splitthebill.server.service.BasicUserAccountService;
+import com.splitthebill.server.service.ThirdPartyUserAccountService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @CrossOrigin(maxAge = 3600)
 @RestController
@@ -32,6 +32,9 @@ public class AuthController {
 
     @NonNull
     BasicUserAccountService basicUserAccountService;
+
+    @NonNull
+    ThirdPartyUserAccountService thirdPartyUserAccountService;
 
     @NonNull
     PasswordEncoder encoder;
@@ -48,16 +51,7 @@ public class AuthController {
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String jwt = jwtUtils.generateJwtTokenForBasicAccount(authentication);
 
-        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-        List<String> roles = userDetails.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.toList());
-
-        return ResponseEntity.ok(new JwtResponseDto(jwt,
-                userDetails.getId(),
-                userDetails.getUsername(),
-                userDetails.getEmail(),
-                roles));
+        return ResponseEntity.ok(new JwtResponseDto(jwt));
     }
 
     @PostMapping("/signup")
@@ -66,7 +60,18 @@ public class AuthController {
         try{
             basicUserAccountService.createUserAccount(userAccount);
         }catch (Exception ignored){}
-
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/oauth2/signin")
+    public ResponseEntity<?> registerThirdPartyUser(@Valid @RequestBody ThirdPartyUserAccountCreateDto userAccount) {
+        ThirdPartyUserAccount account;
+        try {
+            account = thirdPartyUserAccountService.getUserAccountByEmail(userAccount.email);
+        } catch (EntityNotFoundException e) {
+            account = thirdPartyUserAccountService.createUserAccount(userAccount);
+        }
+        String jwt = jwtUtils.generateJwtToken(account.getEmail());
+        return ResponseEntity.ok(new JwtResponseDto(jwt));
     }
 }
