bfaee792a9bded87367bf32301d0f1a42b8a236e
damienthewave <garbalad@gmail.com>
2021-01-23 20:35:54 +0100

    added equals and hashcode to UserAccount class

--- src/main/java/com/splitthebill/server/model/user/UserAccount.java
+++ src/main/java/com/splitthebill/server/model/user/UserAccount.java
@@ -10,6 +10,7 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
---
+++ src/main/java/com/splitthebill/server/repository/GroupExpenseRepository.java
@@ -0,0 +1,2 @@
+package com.splitthebill.server.repository;public interface GroupExpenseRepository {
+}
