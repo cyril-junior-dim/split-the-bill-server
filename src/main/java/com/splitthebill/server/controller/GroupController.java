80f0f380906bc16c62cd1fbef311fd12dbe591ff
damienthewave <garbalad@gmail.com>
2021-01-25 12:17:23 +0100

    Created a get groups endpoint

--- src/main/java/com/splitthebill/server/controller/GroupController.java
+++ src/main/java/com/splitthebill/server/controller/GroupController.java
@@ -3,8 +3,10 @@ package com.splitthebill.server.controller;
 import com.splitthebill.server.dto.group.GroupCreateDto;
 import com.splitthebill.server.dto.group.GroupExpenseCreateDto;
 import com.splitthebill.server.dto.group.GroupReadDto;
+import com.splitthebill.server.dto.group.PersonGroupReadDto;
 import com.splitthebill.server.model.Group;
 import com.splitthebill.server.model.user.Person;
+import com.splitthebill.server.model.user.PersonGroup;
 import com.splitthebill.server.security.JwtUtils;
 import com.splitthebill.server.service.GroupService;
 import lombok.NonNull;
@@ -14,6 +16,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.LinkedList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +29,19 @@ public class GroupController {
 
     @NonNull
     private final JwtUtils jwtUtils;
+
+    @GetMapping
+    public ResponseEntity<?> getUserGroups(Authentication authentication){
+        try {
+            Person person = jwtUtils.getUserAccountFromAuthentication(authentication).getPerson();
+            return ResponseEntity.ok(person.getPersonGroups().stream()
+                    .map(PersonGroupReadDto::new));
+        } catch (NullPointerException e){
+            return ResponseEntity.badRequest().body("There is no person for this account. Create one.");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 
     @PostMapping
     public ResponseEntity<?> createGroup(@Valid @RequestBody GroupCreateDto group) {
--- src/main/java/com/splitthebill/server/dto/group/PersonGroupReadDto.java
+++ src/main/java/com/splitthebill/server/dto/group/PersonGroupReadDto.java
@@ -10,12 +10,14 @@ import java.util.Map;
 @Data
 public class PersonGroupReadDto {
 
+    Long groupId;
     Long groupMemberId;
     Long personId;
     String name;
     Map<Currency, BigDecimal> memberBalance;
 
     public PersonGroupReadDto(PersonGroup member) {
+        this.groupId = member.getGroup().getId();
         this.groupMemberId = member.getId();
         this.personId = member.getPerson().getId();
         this.name = member.getPerson().getName();
