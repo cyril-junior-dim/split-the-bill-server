c65d0aa04bde54f0641376c578185fb23c081eab
damienthewave <garbalad@gmail.com>
2021-01-25 22:00:05 +0100

    Controllers now handle not readable properties

--- src/main/java/com/splitthebill/server/config/ControllerExceptionHandler.java
+++ src/main/java/com/splitthebill/server/config/ControllerExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.splitthebill.server.config;
 
+import org.springframework.beans.NotReadablePropertyException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -16,7 +18,7 @@ import java.util.Map;
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
-    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
@@ -24,6 +26,12 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
             errors.put(fieldName, errorMessage);
         });
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(NotReadablePropertyException.class)
+    public ResponseEntity<?> catchNotReadablePropertyException(){
+        return ResponseEntity.badRequest()
+                .body("One of the properties hasn't been read correctly.");
     }
 
 }
